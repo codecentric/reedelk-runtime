bd3dd8f5ac617223165c9b6398439c4f04a3a4fc
lorenzo <lorenzo@lorenzo.info.com>
2020-01-05 16:12:52 +0100

    code

--- runtime-admin/target/classes/webapp/js/constants.js
+++
@@ -1,6 +0,0 @@
-var Constants = {
-	//ModuleApiPath: 'http://localhost:9988/api/module',
-	//ModuleDeployApiPath: 'http://localhost:9988/api/module/deploy'
-	ModuleApiPath: '/api/module',
-	ModuleDeployApiPath: '/api/module/deploy'
-};
