9a3e6d793b98c943852d1603ac2981136a527eca
lorenzo <lorenzo@lorenzo.info.com>
2020-01-06 17:18:55 +0100

    code

--- runtime-launcher/src/main/java/com/reedelk/runtime/properties/Version.java
+++ runtime-launcher/src/main/java/com/reedelk/runtime/properties/Version.java
@@ -6,7 +6,7 @@ package com.reedelk.runtime.properties;
  */
 public final class Version {
 
-    private static final String VERSION = "0.6.0";
+    private static final String VERSION = "0.6.1-SNAPSHOT";
     private static final String GROUPID = "com.reedelk";
     private static final String ARTIFACTID = "runtime-launcher";
 
