bce708b45032bc822badcff7407b8b0c6a59a2d7
lorenzo <lorenzo@lorenzo.info.com>
2020-02-05 11:26:33 +0100

    code

--- runtime-commons/src/main/java/com/reedelk/runtime/converter/types/defaulttype/AsByteArray.java
+++ runtime-commons/src/main/java/com/reedelk/runtime/converter/types/defaulttype/AsByteArray.java
@@ -3,10 +3,17 @@ package com.reedelk.runtime.converter.types.defaulttype;
 import com.reedelk.runtime.commons.ObjectToBytes;
 import com.reedelk.runtime.converter.types.ValueConverter;
 
+import java.io.Serializable;
+
 class AsByteArray implements ValueConverter<Object,byte[]> {
 
     @Override
     public byte[] from(Object value) {
-        return ObjectToBytes.from(value);
+        if (value instanceof Serializable) {
+            return ObjectToBytes.from(value);
+        } else {
+            String string = value.toString();
+            return string.getBytes();
+        }
     }
 }
