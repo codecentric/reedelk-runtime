14455066962b80fd68490f168dcc0348722df03f
lorenzo <lorenzo@lorenzo.info.com>
2020-03-04 16:17:34 +0100

    code

--- runtime-commons/src/main/java/com/reedelk/runtime/component/ForEach.java
+++ runtime-commons/src/main/java/com/reedelk/runtime/component/ForEach.java
@@ -1,9 +1,6 @@
 package com.reedelk.runtime.component;
 
-import com.reedelk.runtime.api.annotation.Description;
-import com.reedelk.runtime.api.annotation.Example;
-import com.reedelk.runtime.api.annotation.ModuleComponent;
-import com.reedelk.runtime.api.annotation.Property;
+import com.reedelk.runtime.api.annotation.*;
 import com.reedelk.runtime.api.component.Component;
 import com.reedelk.runtime.api.script.dynamicvalue.DynamicObject;
 
@@ -13,6 +10,7 @@ import com.reedelk.runtime.api.script.dynamicvalue.DynamicObject;
 public class ForEach implements Component {
 
     @Property("Collection")
+    @InitValue("#[message.payload()]")
     @Example("<code>message.payload()</code>")
     private DynamicObject collection;
 
