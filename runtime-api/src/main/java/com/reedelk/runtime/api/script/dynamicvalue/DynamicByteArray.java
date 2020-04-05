5b67791e0617ed02eb800ad85a7dda430d82eb91
lorenzo <lorenzo@lorenzo.info.com>
2020-04-05 15:59:47 +0200

    code

--- runtime-api/src/main/java/com/reedelk/runtime/api/script/dynamicvalue/DynamicByteArray.java
+++ runtime-api/src/main/java/com/reedelk/runtime/api/script/dynamicvalue/DynamicByteArray.java
@@ -20,11 +20,22 @@ public class DynamicByteArray extends DynamicValue<byte[]> {
         return new DynamicByteArray(body, context);
     }
 
+    // note that this method can be called if and only if the
+    // dynamic value is NOT a script. If it is a script, the body()
+    // method should be called instead. We can check if it is a script
+    // by using isScript() method from the parent class.
     @Override
     public byte[] value() {
-        return body == null ?
-                new byte[0] :
-                ((String) body).getBytes();
+        if (body == null) {
+            return new byte[0];
+        } else if (body instanceof String) {
+            // A non dynamic byte array is always a string. This is why we check if it
+            // is a string. In this case we do the cast and then return its bytes.
+            return ((String) body).getBytes();
+        } else {
+            throw new IllegalStateException("A non dynamic byte array can only have string type. " +
+                    "If you called this method without checking isScript() before, you should change the implementation.");
+        }
     }
 
     @Override
