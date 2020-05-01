33e15fc835435311420129a5f7b1f9fb5149f085
lorenzo <info@reedelk.com>
2020-05-01 16:35:32 +0200

    Working on input/output descriptors for components + attributes, WIP.

--- module-descriptor/src/main/java/com/reedelk/module/descriptor/analyzer/component/ComponentOutputAnalyzer.java
+++ module-descriptor/src/main/java/com/reedelk/module/descriptor/analyzer/component/ComponentOutputAnalyzer.java
@@ -1,5 +1,6 @@
 package com.reedelk.module.descriptor.analyzer.component;
 
+import com.reedelk.module.descriptor.ModuleDescriptorException;
 import com.reedelk.module.descriptor.model.component.ComponentOutputDescriptor;
 import com.reedelk.runtime.api.annotation.ComponentOutput;
 import com.reedelk.runtime.api.commons.StringUtils;
@@ -12,6 +13,7 @@ import java.util.List;
 
 import static com.reedelk.module.descriptor.analyzer.commons.ScannerUtils.getParameterValue;
 import static com.reedelk.module.descriptor.analyzer.commons.ScannerUtils.hasAnnotation;
+import static java.lang.String.format;
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
 
@@ -32,6 +34,10 @@ public class ComponentOutputAnalyzer {
         String attributesType = getParameterValue("attributes", MessageAttributes.class.getName(), annotationInfo);
         String description = getParameterValue("description", StringUtils.EMPTY, annotationInfo);
         List<String> outputPayload = getOutputPayload(annotationInfo);
+        if (outputPayload.isEmpty()) {
+            String error = format("Component Output payload types must not be empty (class: %s).", classInfo.getName());
+            throw new ModuleDescriptorException(error);
+        }
 
         ComponentOutputDescriptor descriptor = new ComponentOutputDescriptor();
         descriptor.setAttributes(attributesType);
---
+++ module-descriptor/src/test/java/com/reedelk/module/descriptor/analyzer/component/ComponentOutputAnalyzerComponentPayloadEmpty.java
@@ -0,0 +1,7 @@
+package com.reedelk.module.descriptor.analyzer.component;
+
+import com.reedelk.runtime.api.annotation.ComponentOutput;
+
+@ComponentOutput(payload = {})
+public class ComponentOutputAnalyzerComponentPayloadEmpty {
+}
