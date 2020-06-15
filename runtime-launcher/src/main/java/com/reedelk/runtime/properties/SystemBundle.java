829b582b42acb8b789e06f513cdec83bed07f3a1
lorenzo <info@reedelk.com>
2020-06-15 13:08:03 +0200

    code

--- runtime-launcher/src/main/java/com/reedelk/runtime/properties/SystemBundle.java
+++ runtime-launcher/src/main/java/com/reedelk/runtime/properties/SystemBundle.java
@@ -17,11 +17,11 @@ public class SystemBundle {
                 "pax-logging-api-1.11.0.jar",
                 "pax-logging-logback-1.11.0.jar",
                 "json-20190722.jar",
-                "httpcore-osgi-4.4.12.jar",
-                "httpclient-osgi-4.5.10.jar",
+                "httpcore-osgi-4.4.13.jar",
+                "httpclient-osgi-4.5.12.jar",
                 "httpasyncclient-osgi-4.1.4.jar",
                 "reactive-streams-1.0.3.jar",
-                "reactor-core-3.2.16.RELEASE.jar",
+                "reactor-core-3.2.18.RELEASE.jar",
                 "runtime-api.jar", // NAME_CONVENTION
                 "runtime-commons.jar", // NAME_CONVENTION
                 "runtime-platform.jar")); // NAME_CONVENTION
