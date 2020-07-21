e6c0fb6b0d29b01b8b50a5a70456e890ea867100
lorenzo <info@reedelk.com>
2020-07-21 14:58:06 +0200

    OpenAPI library.

--- runtime-openapi/src/main/java/com/reedelk/runtime/openapi/v3/OpenApiSerializable.java
+++ runtime-openapi/src/main/java/com/reedelk/runtime/openapi/v3/OpenApiSerializable.java
@@ -1,5 +1,6 @@
 package com.reedelk.runtime.openapi.v3;
 
+import com.reedelk.runtime.openapi.v3.model.OpenApiObject;
 import org.json.JSONObject;
 import org.yaml.snakeyaml.Yaml;
 
@@ -10,6 +11,11 @@ public interface OpenApiSerializable {
     int JSON_INDENT_FACTOR = 2;
 
     Map<String,Object> serialize(OpenApiSerializableContext context);
+
+    default OpenApiObject fromYaml(String yamlAsString) {
+        Yaml yaml = new Yaml();
+        return yaml.loadAs(yamlAsString, OpenApiObject.class);
+    }
 
     default String toJson(OpenApiSerializableContext context) {
         Map<String, Object> serialized = serialize(context);
@@ -24,6 +30,7 @@ public interface OpenApiSerializable {
 
     // REST Listener Model
     default String toRestListenerModel(OpenApiSerializableContext context) {
+        // There is one object containing
         Map<String, Object> serialized = serialize(context);
         return new JSONObject(serialized).toString(JSON_INDENT_FACTOR);
     }
