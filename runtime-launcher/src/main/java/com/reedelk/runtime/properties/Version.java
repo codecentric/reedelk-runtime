8693503b6f4a4874d338fd74db9b87c0fcd3f31d
lorenzo <info@reedelk.com>
2020-08-26 18:46:04 +0200

    misc: added README.md.

--- runtime-launcher/src/main/java/com/reedelk/runtime/properties/Version.java
+++ runtime-launcher/src/main/java/com/reedelk/runtime/properties/Version.java
@@ -6,7 +6,7 @@ package com.reedelk.runtime.properties;
  */
 public final class Version {
 
-    private static final String VERSION = "1.0.2";
+    private static final String VERSION = "1.0.3-SNAPSHOT";
     private static final String GROUPID = "com.reedelk";
     private static final String ARTIFACTID = "runtime-launcher";
     private static final String QUALIFIER = "CE";
