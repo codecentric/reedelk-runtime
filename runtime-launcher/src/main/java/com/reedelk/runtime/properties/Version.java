88177e4f0c046b46ae872cfdb37c9f0cc5fc2d29
lorenzo <info@reedelk.com>
2020-10-06 11:39:06 +0200

    misc: preparing release 1.0.6

--- runtime-launcher/src/main/java/com/reedelk/runtime/properties/Version.java
+++ runtime-launcher/src/main/java/com/reedelk/runtime/properties/Version.java
@@ -6,7 +6,7 @@ package com.reedelk.runtime.properties;
  */
 public final class Version {
 
-    private static final String VERSION = "1.0.5";
+    private static final String VERSION = "1.0.6";
     private static final String GROUPID = "com.reedelk";
     private static final String ARTIFACTID = "runtime-launcher";
     private static final String QUALIFIER = "CE";
